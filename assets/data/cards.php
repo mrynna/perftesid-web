cf6fef994df72abbe5a2bab246f72a04494c9e90
mrynna <muhrianardiansah@gmail.com>
2023-07-04 11:44:54 +0800

    separate data card to different file from index.php

---
+++ assets/data/cards.php
@@ -0,0 +1,98 @@
+<?php
+    include ('../../api/connection.php');
+    include ('../../api/service.data.php');
+?>
+
+<div class="row">
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <h4 class="text-muted fw-normal mt-0 text-truncate" title="Campaign Sent">Tegangan (Voc)</h4>
+                        <h3 class="my-2 py-1">
+                            <?= $tegangan; ?> V
+                        </h3>
+                        <p class="mb-0 text-muted">
+                            <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> Volt</span>
+                        </p>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end card-body -->
+        </div> <!-- end card -->
+    </div> <!-- end col -->
+
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <h4 class="text-muted fw-normal mt-0 text-truncate" title="New Leads">Arus</h4>
+                        <h3 class="my-2 py-1">
+                            <?= $arus; ?> A
+                        </h3>
+                        <p class="mb-0 text-muted">
+                            <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> Ampere</span>
+                        </p>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end card-body -->
+        </div> <!-- end card -->
+    </div> <!-- end col -->
+
+
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <h4 class="text-muted fw-normal mt-0 text-truncate" title="Deals">Suhu Panel</h4>
+                        <h3 class="my-2 py-1">
+                            <?= $suhuPanel; ?> C
+                        </h3>
+                        <p class="mb-0 text-muted">
+                            <span class="text-danger me-2"><i class="mdi mdi-arrow-up-bold"></i> Celcius</span>
+                        </p>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end card-body -->
+        </div> <!-- end card -->
+    </div> <!-- end col -->
+
+    <div class="col-lg-6 col-xl-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-6">
+                        <h4 class="text-muted fw-normal mt-0 text-truncate" title="Booked Revenue">Suhu Lingkungan</h4>
+                        <h3 class="my-2 py-1">
+                            <?= $suhuLingkungan; ?> C
+                        </h3>
+                        <p class="mb-0 text-muted">
+                            <span class="text-danger me-2"><i class="mdi mdi-arrow-up-bold"></i> Celcius</span>
+                        </p>
+                    </div>
+                </div> <!-- end row-->
+            </div> <!-- end card-body -->
+        </div> <!-- end card -->
+    </div> <!-- end col -->
+</div>
+<!-- end row -->
+
+<div class="col-lg-6 col-xl-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="row align-items-center">
+                <div class="col-6">
+                    <h4 class="text-muted fw-normal mt-0 text-truncate" title="New Leads">Iradiasi</h4>
+                    <h3 class="my-2 py-1">
+                        <?= $iradiasi; ?> w/m2
+                    </h3>
+                    <p class="mb-0 text-muted">
+                        <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> w/m2</span>
+                    </p>
+                </div>
+            </div> <!-- end row-->
+        </div> <!-- end card-body -->
+    </div> <!-- end card -->
+</div> <!-- end col -->
--- index.php
+++ index.php
@@ -1,9 +1,4 @@
-﻿<?php
-    include ('api/connection.php');
-    include ('api/service.data.php');
-?>
-
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
     <html lang="en">
 
     <head>
@@ -280,94 +275,25 @@
                             </div>
                         </div>
                         <!-- end page title -->
-
+                        <div class="container" id="card-data"></div>
+                        <script>
+                            function loadXMLDoc(){
+                                var card = new XMLHttpRequest();
+                                card.onreadystatechange = function() {
+                                    if (this.readyState == 4 && this.status == 200) {
+                                    // Typical action to be performed when the document is ready:
+                                    document.getElementById("card-data").innerHTML = card.responseText;
+                                    }
+                                };
+                                card.open("GET", "assets/data/cards.php", true);
+                                card.send();
+                            }
+                            setInterval(function(){
+                                loadXMLDoc();
+                            }, 1000);
+                            window.onload = loadXMLDoc();
+                        </script>
                         <div class="row">
-                            <div class="col-lg-6 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row align-items-center">
-                                            <div class="col-6">
-                                                <h4 class="text-muted fw-normal mt-0 text-truncate" title="Campaign Sent">Tegangan (Voc)</h4>
-                                                <h3 class="my-2 py-1"><?=$tegangan;?> V</h3>
-                                                <p class="mb-0 text-muted">
-                                                    <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> Volt</span>
-                                                </p>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div> <!-- end card-body -->
-                                </div> <!-- end card -->
-                            </div> <!-- end col -->
-        
-                            <div class="col-lg-6 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row align-items-center">
-                                            <div class="col-6">
-                                                <h4 class="text-muted fw-normal mt-0 text-truncate" title="New Leads">Arus</h4>
-                                                <h3 class="my-2 py-1"><?=$arus;?> A</h3>
-                                                <p class="mb-0 text-muted">
-                                                    <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> Ampere</span>
-                                                </p>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div> <!-- end card-body -->
-                                </div> <!-- end card -->
-                            </div> <!-- end col -->
-
-                            
-                            <div class="col-lg-6 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row align-items-center">
-                                            <div class="col-6">
-                                                <h4 class="text-muted fw-normal mt-0 text-truncate" title="Deals">Suhu Panel</h4>
-                                                <h3 class="my-2 py-1"><?=$suhuPanel;?> C</h3>
-                                                <p class="mb-0 text-muted">
-                                                    <span class="text-danger me-2"><i class="mdi mdi-arrow-up-bold"></i> Celcius</span>
-                                                </p>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div> <!-- end card-body -->
-                                </div> <!-- end card -->
-                            </div> <!-- end col -->
-                            
-                            <div class="col-lg-6 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row align-items-center">
-                                            <div class="col-6">
-                                                <h4 class="text-muted fw-normal mt-0 text-truncate" title="Booked Revenue">Suhu Lingkungan</h4>
-                                                <h3 class="my-2 py-1"><?=$suhuLingkungan;?> C</h3>
-                                                <p class="mb-0 text-muted">
-                                                    <span class="text-danger me-2"><i class="mdi mdi-arrow-up-bold"></i> Celcius</span>
-                                                </p>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div> <!-- end card-body -->
-                                </div> <!-- end card -->
-                            </div> <!-- end col -->
-                        </div>
-                        <!-- end row -->
-                        
-                        <div class="col-lg-6 col-xl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="row align-items-center">
-                                        <div class="col-6">
-                                            <h4 class="text-muted fw-normal mt-0 text-truncate" title="New Leads">Iradiasi</h4>
-                                            <h3 class="my-2 py-1"><?=$iradiasi;?> w/m2</h3>
-                                            <p class="mb-0 text-muted">
-                                                <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> w/m2</span>
-                                            </p>
-                                        </div>
-                                    </div> <!-- end row-->
-                                </div> <!-- end card-body -->
-                            </div> <!-- end card -->
-                        </div> <!-- end col -->
-                        
-                        <div class="row">
-                            
-
                             <div class="col-xl-7 col-lg-6">
                                 <div class="card card-h-100">
                                     <div class="card-body">

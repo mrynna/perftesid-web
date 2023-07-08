9c7e1edb144ecfe40bc51582aa00094502c9ef3a
mrynna <muhrianardiansah@gmail.com>
2023-07-09 02:02:40 +0800

    Fix where Tegangan Chart on Grafik Data menu only shows 10 data

--- api/service.chart.php
+++ api/service.chart.php
@@ -3,12 +3,7 @@ session_start();
 if (isset($_SESSION['user_id']) && isset($_SESSION['user_email'])) {
     require 'connection.php';
     $token = $_SESSION['user_token'];
-    $sqlID = $dbconnect->query("SELECT MAX(id) FROM tb_data WHERE token = '$token'");
-    $dataID = $sqlID->fetch_assoc();
-    $idAkhir = $dataID['MAX(id)'];
-    $idAwal = $idAkhir-9;
-
-    $sql_asc = "SELECT * FROM tb_data WHERE token = '$token' and id >= $idAwal and id <= $idAkhir ORDER BY id ASC";
+    $sql_asc = "SELECT * FROM tb_data WHERE token = '$token' ORDER BY id ASC";
 
     // Execute the query and fetch the results 
     $result_asc = $dbconnect->query($sql_asc);
